08713a0763657f450920ec2808b0f56c5fc7b37f
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-02 21:43:36 +0600

    port is running

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const express = require("express")
+const app = express()
+
+
+
+
+
+
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const app = require("./app")
+const PORT = process.env.API_PORT || 5000;
+
+
+
+
+app.listen(PORT, () => [
+    console.log(`app is running ${PORT}`)
+])
