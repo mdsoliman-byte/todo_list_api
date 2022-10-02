4173e9e85e5ea46ac5dc6f7c1f236f47e9d3397b
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-02 22:33:36 +0600

    all package install complite

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app")
 const env = require('dotenv').config()
 const PORT = process.env.API_PORT ;
-console.log("hellloo")
+
 app.listen(PORT, () => [
     console.log(`app is running ${PORT}`)
 ])
