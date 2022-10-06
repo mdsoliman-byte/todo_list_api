68d2fad236980c3b78c10039c705bd46bffa1a15
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-07 02:50:57 +0600

    user create success for unique email and uniqu username

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const dataSchema = mongoose.Schema({
     FirstName: { type: String },
     LastName: { type: String },
-    EmailAddress: { type: String },
+    EmailAddress: { type: String, unique: true },
     MobileNumber: { type: String },
     City: { type: String },
-    UserName: { type: String , unique: true },
+    UserName: { type: String, unique: true },
     Password: { type: String },
 
 }, { versionKye: false })
