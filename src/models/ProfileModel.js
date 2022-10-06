7138221f58ddff712bd6ab3aa0dc6ad565fdaa49
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-07 01:09:06 +0600

    user profile data schima created done

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 
 const dataSchema = mongoose.Schema({
+    FirstName: { type: String },
+    LastName: { type: String },
+    EmailAddress: { type: String },
+    MobileNumber: { type: String },
+    City: { type: String },
+    UserName: { type: String },
+    Password: { type: String },
 
 }, { versionKye: false })
 
